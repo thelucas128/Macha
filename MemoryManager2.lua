0840051458055cd727a3dc7aee2a55600041ef26
thelucas128 <109916070+thelucas128@users.noreply.github.com>
2026-01-09 08:33:02 +0100

    Add functions to get rotation matrix and Euler angles

--- MemoryManager2.lua
+++ MemoryManager2.lua
@@ -48,4 +48,60 @@ function MemoryManager.GetImageId(Address)
     return memory_read("string", memory_read("uintptr_t", Address + offsets.GuiObject.Image) + 0x0)
 end
 
+function MemoryManager.GetRotationMatrix(part)
+    if not part then
+        return nil
+    end
+    
+    local address = part.Address or part
+    if not address then
+        return nil
+    end
+    
+    local primitive = memory_read("uintptr_t", address + offsets.BasePart.Primitive)
+    if not primitive then
+        return nil
+    end
+    
+    
+    local m = {}
+    for i = 0, 8 do
+        m[i] = memory_read("float", primitive + offsets.BasePart.Rotation  + (i * 0x04))
+    end
+    
+    return m
+end
+
+function MemoryManager.GetEulerAngles(part)
+    local m = MemoryManager.GetRotationMatrix(part)
+    if not m then
+        return nil
+    end
+    
+    local m21 = -m[5]
+    local pitch, yaw, roll
+    
+    if m21 < 0.99999 then
+        if m21 > -0.99999 then
+            pitch = math.asin(m21)
+            yaw = math.atan2(-m[2], -m[8])
+            roll = math.atan2(-m[3], -m[4])
+        else
+            pitch = math.pi / 2
+            yaw = -math.atan2(m[6], m[0])
+            roll = 0
+        end
+    else
+        pitch = -math.pi / 2
+        yaw = math.atan2(m[6], m[0])
+        roll = 0
+    end
+    
+    return {
+        Yaw = yaw,
+        Pitch = pitch,
+        Roll = roll
+    }
+end
+
 return MemoryManager
